fd24c8859ddd5196a2ae7c76683adeb28084a24b
sberoch <sberoch@gmail.com>
2020-07-13 15:31:49 -0300

    refactor topo

--- tp3/topology/fat-tree.py
+++ tp3/topology/fat-tree.py
@@ -10,40 +10,49 @@ from mininet.topo import Topo
 class FatTree( Topo ):
   def __init__( self, half_ports = 2, **opts ):
     Topo.__init__(self, **opts)
-
-    # Primero creamos los dos switches
     
+    sw00 = self.create_root()
 
-    # Creamos el primer switch
+    switches = self.create_switches([[sw00]], opts['levels'])
+    
+    self.add_last_level_hosts(switches)
+
+
+  def create_root(self):
     sw00 = self.addSwitch('sw00')
 
-    # Luego creamos los tres hosts
     h1 = self.addHost('h1')
     h2 = self.addHost('h2')
     h3 = self.addHost('h3')
-
 
     self.addLink(sw00, h1)
     self.addLink(sw00, h2)
     self.addLink(sw00, h3)
 
-    switches = [[sw00]]
-    for i in range(1,opts['levels']):
-        switches_nivel = []
-        for j in range(2**i):
-            switches_nivel.append(self.addSwitch('sw'+str(i)+str(j)))
-        switches.append(switches_nivel)
+    return sw00
+
+
+  def create_switches(self, switches, levels):
+    for i in range(1, levels):
+      switches_nivel = []
+      for j in range(2**i):
+        switches_nivel.append(self.addSwitch('sw'+str(i)+str(j)))
+      switches.append(switches_nivel)
 
     for i in range(len(switches)-1):
-        for j in range(len(switches[i])):
-            nivel_siguiente = switches[i+1]
-            for sw in nivel_siguiente:
-                self.addLink(switches[i][j], sw)
+      for j in range(len(switches[i])):
+        nivel_siguiente = switches[i+1]
+        for sw in nivel_siguiente:
+          self.addLink(switches[i][j], sw)
+    return switches
 
+
+  def add_last_level_hosts(self, switches):
     host_number = 4
     for sw in switches[-1]:
-        self.addHost('h'+str(host_number))
-        self.addLink(sw,'h'+str(host_number))
-        host_number+=1
+      self.addHost('h'+str(host_number))
+      self.addLink(sw,'h'+str(host_number))
+      host_number+=1
+
 
 topos = { 'fat-tree': FatTree }
