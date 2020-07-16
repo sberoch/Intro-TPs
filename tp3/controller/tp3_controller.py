3c079dec80aec3ddd41a33b41ec5a3dbdcfd4485
franco-giordano <francogior98@gmail.com>
2020-07-16 00:31:20 -0300

    si

--- tp3/controller/tp3_controller.py
+++ tp3/controller/tp3_controller.py
@@ -6,11 +6,16 @@ import pox.lib.packet as pkt
 
 import networkx as nx
 
+import pox.openflow.libopenflow_01 as of
+
+
 class Tp3Controller:
 	def __init__(self):
 		self.connections = set()
 		self.switches = []
 		self.graph = nx.Graph()
+		self.hosts = set()
+		self.output_ports = {}
 		core.call_when_ready(self.start, ('openflow', 'openflow_discovery'))
 
 	def start(self):
@@ -30,13 +35,44 @@ class Tp3Controller:
 	def _handle_LinkEvent(self, event):
 		print('---Link found!')
 		self.graph.add_edge(event.link.dpid1, event.link.dpid2)
-		print(nx.to_dict_of_lists(self.graph))
+		
+		self.output_ports[(event.link.dpid1, event.link.dpid2)] = event.link.port1
+		print(self.output_ports)
 
 
 	def _handle_PacketIn(self, event):
 		print('--Packet came in:', event.port) #, event.parsed.ipv4.srcip)
 
-		print([p for p in nx.all_shortest_paths(self.graph, source=1, target=4)])
+		#print([p for p in nx.all_shortest_paths(self.graph, source=1, target=4)])
+		ipv4 = event.parsed.find('ipv4')
+		
+		if ipv4 is None:
+			return
+
+		srcip = str(ipv4.srcip)
+		dstip = str(ipv4.dstip)
+
+		if srcip not in self.hosts:
+			self.hosts.add(srcip)
+			self.graph.add_edge(srcip, event.connection.dpid)
+			self.output_ports[(event.connection.dpid, srcip)] = event.port
+
+		if dstip not in self.graph:
+			return
+
+		current_path = nx.all_shortest_paths(self.graph, source=event.connection.dpid, target=dstip).next()
+		print("PATH:", current_path)
+		print("hosts:", self.hosts)
+		next_hop = current_path[1]
+
+		msg = of.ofp_flow_mod()
+		msg.match.nw_dst = ipv4.dstip
+		msg.match.dl_type=0x800
+		#msg.match.tp_dst = 
+		msg.actions.append(of.ofp_action_output(port = self.output_ports[(event.connection.dpid, next_hop)]))
+		event.connection.send(msg)
+
+		#return generate_ft(self.paths, dpid) 
 
 
 def launch():
